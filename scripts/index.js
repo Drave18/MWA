397818de43e12109d7d436329441595be96951b6
Drave <davidsebas1825@gmail.com>
2024-01-06 10:19:09 -0500

    Se finalizo la web

---
+++ scripts/index.js
@@ -0,0 +1,111 @@
+ScrollReveal().reveal(".hero", {
+  delay: 300,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  interval: 100,
+});
+
+ScrollReveal().reveal(".call_to_action", {
+  delay: 600,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  interval: 100,
+});
+
+ScrollReveal().reveal(".motivational", {
+  delay: 300,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  interval: 100,
+});
+
+ScrollReveal().reveal(".mwagency__title", {
+  delay: 200,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  interval: 100,
+});
+
+ScrollReveal().reveal(".mwagency__subtitle", {
+  delay: 300,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  interval: 100,
+});
+
+ScrollReveal().reveal(".card", {
+  delay: 200, // Retardo entre cada carta
+  distance: "50px", // Distancia de animación
+  origin: "bottom", // Origen de la animación
+  easing: "ease-in-out", // Tipo de easing
+  interval: 200, // Intervalo entre cada carta
+});
+
+ScrollReveal().reveal(".workprooves__paragraph", {
+  delay: 300,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  interval: 100,
+});
+
+ScrollReveal().reveal(".workprooves__images", {
+  delay: 350,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  interval: 200,
+});
+
+ScrollReveal().reveal(".datapoint__title", {
+  delay: 400,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  interval: 100,
+});
+
+ScrollReveal().reveal(".datapoint__paragraph", {
+  delay: 450,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  interval: 100,
+});
+
+ScrollReveal().reveal(".services__title", {
+  delay: 450,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  interval: 100,
+});
+
+ScrollReveal().reveal(".text-box", {
+  delay: 500, // Retardo entre cada carta
+  distance: "50px", // Distancia de animación
+  origin: "bottom", // Origen de la animación
+  easing: "ease-in-out", // Tipo de easing
+  interval: 200, // Intervalo entre cada carta
+});
+
+ScrollReveal().reveal(".image", {
+  delay: 500, // Retardo entre cada carta
+  distance: "50px", // Distancia de animación
+  origin: "bottom", // Origen de la animación
+  easing: "ease-in-out", // Tipo de easing
+  interval: 200, // Intervalo entre cada carta
+});
+
+ScrollReveal().reveal(".boxes", {
+  delay: 200, // Retardo entre cada carta
+  distance: "50px", // Distancia de animación
+  origin: "bottom", // Origen de la animación
+  easing: "ease-in-out", // Tipo de easing
+  interval: 200, // Intervalo entre cada carta
+});
